d2f1849ddf9fe5d65233dc990c751201f3b8667f
ruoxuanFu <renpizhu@126.com>
2022-09-22 17:16:20 +0800

    Create Md5Util.java

---
+++ code shop/Md5Util.java
@@ -0,0 +1,30 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+/**
+ * MD5工具类
+ *
+ * @author furuoxuan
+ */
+public class Md5Util {
+
+    public static String md5(String string) {
+        byte[] hash;
+
+        try {
+            hash = MessageDigest.getInstance("MD5").digest(string.getBytes(StandardCharsets.UTF_8));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        StringBuilder hex = new StringBuilder(hash.length * 2);
+
+        for (byte b : hash) {
+            if ((b & 0xFF) < 0x10) {
+                hex.append("0");
+            }
+            hex.append(Integer.toHexString(b & 0xFF));
+        }
+        return hex.toString();
+    }
+}
